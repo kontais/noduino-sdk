40bcf9426a9ea99f25db2cfcdaf42ce62d137ea3
Jack Tan <jiankemeng@gmail.com>
2016-03-17 21:59:47 +0800

    example/noduino: update the build system

--- examples/noduino/mjyun/main.c
+++ examples/noduino/mjyun/main.c
@@ -23,58 +23,58 @@
 irom void mjyun_stated_cb(mjyun_state_t state)
 {
     if (mjyun_state() != state)
-        os_printf("Platform: mjyun_state error \r\n");
+        serial_printf("Platform: mjyun_state error \r\n");
 
     switch (state)
     {
         case WIFI_IDLE:
-            os_printf("Platform: WIFI_IDLE\r\n");
+            serial_printf("Platform: WIFI_IDLE\r\n");
             break;
         case WIFI_SMARTLINK_LINKING:
-            os_printf("Platform: WIFI_SMARTLINK_LINKING\r\n");
+            serial_printf("Platform: WIFI_SMARTLINK_LINKING\r\n");
             break;
         case WIFI_SMARTLINK_FINDING:
-            os_printf("Platform: WIFI_SMARTLINK_FINDING\r\n");
+            serial_printf("Platform: WIFI_SMARTLINK_FINDING\r\n");
             break;
         case WIFI_SMARTLINK_TIMEOUT:
-            os_printf("Platform: WIFI_SMARTLINK_TIMEOUT\r\n");
+            serial_printf("Platform: WIFI_SMARTLINK_TIMEOUT\r\n");
             break;
         case WIFI_SMARTLINK_GETTING:
-            os_printf("Platform: WIFI_SMARTLINK_GETTING\r\n");
+            serial_printf("Platform: WIFI_SMARTLINK_GETTING\r\n");
             break;
         case WIFI_SMARTLINK_OK:
-            os_printf("Platform: WIFI_SMARTLINK_OK\r\n");
+            serial_printf("Platform: WIFI_SMARTLINK_OK\r\n");
             break;
         case WIFI_AP_OK:
-            os_printf("Platform: WIFI_AP_OK\r\n");
+            serial_printf("Platform: WIFI_AP_OK\r\n");
             break;
         case WIFI_AP_ERROR:
-            os_printf("Platform: WIFI_AP_ERROR\r\n");
+            serial_printf("Platform: WIFI_AP_ERROR\r\n");
             break;
         case WIFI_AP_STATION_OK:
-            os_printf("Platform: WIFI_AP_STATION_OK\r\n");
+            serial_printf("Platform: WIFI_AP_STATION_OK\r\n");
             break;
         case WIFI_AP_STATION_ERROR:
-            os_printf("Platform: WIFI_AP_STATION_ERROR\r\n");
+            serial_printf("Platform: WIFI_AP_STATION_ERROR\r\n");
             break;
         case WIFI_STATION_OK:
-            os_printf("Platform: WIFI_STATION_OK\r\n");
+            serial_printf("Platform: WIFI_STATION_OK\r\n");
             break;
         case WIFI_STATION_ERROR:
-            os_printf("Platform: WIFI_STATION_ERROR\r\n");
+            serial_printf("Platform: WIFI_STATION_ERROR\r\n");
             break;
         case MJYUN_CONNECTING:
-            os_printf("Platform: MJYUN_CONNECTING\r\n");
+            serial_printf("Platform: MJYUN_CONNECTING\r\n");
             break;
         case MJYUN_CONNECTING_ERROR:
-            os_printf("Platform: MJYUN_CONNECTING_ERROR\r\n");
+            serial_printf("Platform: MJYUN_CONNECTING_ERROR\r\n");
             break;
         case MJYUN_CONNECTED:
-            os_printf("Platform: MJYUN_CONNECTED \r\n");
+            serial_printf("Platform: MJYUN_CONNECTED \r\n");
 			digitalWrite(2, LOW);
             break;
         case MJYUN_DISCONNECTED:
-            os_printf("Platform: MJYUN_DISCONNECTED\r\n");
+            serial_printf("Platform: MJYUN_DISCONNECTED\r\n");
 			digitalWrite(2, HIGH);
             break;
         default:
@@ -84,7 +84,7 @@ irom void mjyun_stated_cb(mjyun_state_t state)
 
 irom void mjyun_receive(const char *event_name, const char *event_data)
 {
-	os_printf("RECEIVED: key:value [%s]:[%s]", event_name, event_data);
+	serial_printf("RECEIVED: key:value [%s]:[%s]", event_name, event_data);
 	// Publish back
 	mjyun_publish(event_name, event_data);
 }
@@ -100,19 +100,20 @@ void mjyun_disconnected()
     digitalWrite(2, HIGH);
 }
 
+/*
+ * 3707 --> 摩羯插座
+ * 3708 --> 摩羯灯
+ */
 const mjyun_config_t mjyun_conf = {
-	"WotP0123456789",  /* 产品id [必填]*/
-	//"gh_51111441aa63",  /* 产品id [必填]*/
-	NULL,/*产品子id(一般用于微信设备) [选填]*/
-	NULL,/*设备上线时，给app发送online消息中的附加数据，[选填]*/
-	NULL/*设备掉线时，给app发送offline消中的附加数据，[选填]*/
+	//"WotP0123456789",		/* 产品id [必填] */
+	"gh_51111441aa63",		/* 产品id [必填] */
+	"3707",					/* 产品子id (一般用于微信设备) [选填]*/
+	"Hi, I'm coming!!!",	/* 设备上线时，给app发送 online 消息中的附加数据，[选填] */
+	"I will come back!!!"	/* 设备掉线时，给app发送 offline 消息中的附加数据，[选填] */
 };
 
-irom void setup()
+void init_yun()
 {
-	serial_begin(115200);
-	pinMode(2, OUTPUT);
-
 	mjyun_statechanged(mjyun_stated_cb);
 	mjyun_ondata(mjyun_receive);
 	mjyun_onconnected(mjyun_connected);
@@ -121,8 +122,16 @@ irom void setup()
 	mjyun_run(&mjyun_conf);
 }
 
+irom void setup()
+{
+	serial_begin(115200);
+	pinMode(2, OUTPUT);
+
+	init_yun();
+}
+
 irom void loop()
 {
-	serial_printf("Hello World!\r\n");
-	delay(1500);
+	serial_printf("MJYUN device heart beat!\r\n");
+	delay(2000);
 }
