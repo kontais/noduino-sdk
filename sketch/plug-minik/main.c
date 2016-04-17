46b0819deb8f93314a9ba1257a2b0b0b6cd82fa0
Jack Tan <jiankemeng@gmail.com>
2016-04-17 23:14:28 +0800

    sketch/plug-minik: use the mini ota function of libmjyun

--- sketch/plug-minik/main.c
+++ sketch/plug-minik/main.c
@@ -103,6 +103,13 @@ void mjyun_receive(const char *event_name, const char *event_data)
 		param_save();
 		relay_set_status_and_publish(0);
 	}
+	if(os_strncmp(event_data, "ota", 3) == 0)
+	{
+#ifdef DEBUG
+		os_printf("OTA: upgrade the firmware!\r\n");
+#endif
+		mini_ota_start("ota/dev/minik/files");
+	}
 }
 
 void mjyun_connected()
