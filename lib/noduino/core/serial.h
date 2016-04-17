2deccfb98f1a9b8caa018facb165c8745a21e269
Jack Tan <jiankemeng@gmail.com>
2016-04-17 23:18:08 +0800

    lib/noduino: update the implemetation of serial
    To fix the dummy crash issue

--- lib/noduino/core/serial.h
+++ lib/noduino/core/serial.h
@@ -23,7 +23,6 @@
 
 #include "noduino.h"
 
-// Define config for Serial.begin(baud, config);
 #define SERIAL_5N1 0x10
 #define SERIAL_6N1 0x14
 #define SERIAL_7N1 0x18
@@ -53,33 +52,6 @@
 #define SERIAL_RX_ONLY  1
 #define SERIAL_TX_ONLY  2
 
-#define	UART0		0
-#define	UART1		1
-#define UART_NO		-1
-
-#define UART_TX_FIFO_SIZE	0x80
-#define RING_BUFFER_SIZE	256
-
-/* FIFO Ring Buffer */
-typedef struct ring_buffer {
-	unsigned char buffer[RING_BUFFER_SIZE];
-	volatile unsigned int head;
-	volatile unsigned int tail;
-} ring_buffer_t;
-
-typedef struct uart_ {
-	int uart_nr;
-	int baud_rate;
-	bool rxEnabled;
-	bool txEnabled;
-	uint8_t rxPin;
-	uint8_t txPin;
-
-	ring_buffer_t *_tx_buffer;
-	ring_buffer_t *_rx_buffer;
-	bool _written;
-} uart_t;
-
 void serial_begin(unsigned long baud);
 void serial_end();
 void serial_flush();
@@ -100,32 +72,5 @@ size_t serial1_write(char c);
 void serial1_print(const char *buf);
 #define	serial1_printf(x, ...)		\
 	do{char b[128]={0};sprintf(b,x,##__VA_ARGS__);serial1_print(b);}while(0)
-
-void uart_set_debug(int uart_nr);
-int uart_get_debug();
-
-void uart_interrupt_handler(uart_t * uart);
-void serial_rx_complete_irq(uart_t * uart, char c);
-void serial_tx_empty_irq(uart_t * uart);
-void uart_wait_for_tx_fifo(uart_t * uart, size_t size_needed);
-size_t uart_get_tx_fifo_room(uart_t * uart);
-void uart_wait_for_transmit(uart_t * uart);
-void uart_transmit_char(uart_t * uart, char c);
-void uart_transmit(uart_t * uart, const char *buf, size_t size);
-void uart_flush(uart_t * uart);
-void uart_interrupt_enable(uart_t * uart);
-void uart_interrupt_disable(uart_t * uart);
-void uart_arm_tx_interrupt(uart_t * uart);
-void uart_disarm_tx_interrupt(uart_t * uart);
-void uart_set_baudrate(uart_t * uart, int baud_rate);
-int uart_get_baudrate(uart_t * uart);
-
-uart_t *uart_init(int uart_nr, int baudrate, int config, int mode);
-void uart_uninit(uart_t * uart);
-void uart_swap(uart_t * uart);
-
-void uart_ignore_char(char c);
-void uart0_write_char(char c);
-void uart1_write_char(char c);
 
 #endif
